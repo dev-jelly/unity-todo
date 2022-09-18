76b471f8de976eb1d6df9347af743de5b0a1feda
jelly <chm073@gmail.com>
2022-09-18 16:03:20 +0900

    Add Text Strike

--- Assets/Todo/Runtime/UI/TodoItem.cs
+++ Assets/Todo/Runtime/UI/TodoItem.cs
@@ -1,24 +1,33 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UIElements;
 using Image = UnityEngine.UI.Image;
+using Toggle = UnityEngine.UI.Toggle;
 
 public class TodoItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    public Toggle toggle;
     public TMP_Text text;
-
     public Image image;
-    
-    
+
     public void OnPointerEnter(PointerEventData eventData)
     {
-        image.color = new Color(100, 100, 100, 100f);
+        image.color = new Color(1, 1, 1, 1);
     }
 
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        image.color = new Color(255,255,255, 0.1f);
+        image.color = new Color(1, 1, 1, 0.1f);
+    }
+
+    private void Start()
+    {
+        toggle.onValueChanged.AddListener(OnToggleValueChanged);
+    }
+
+    private void OnToggleValueChanged(bool isOn)
+    {
+        text.fontStyle = isOn ? FontStyles.Strikethrough : FontStyles.Normal;
     }
 }
